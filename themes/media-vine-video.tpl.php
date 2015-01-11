dfee8608e12424049bf37aa490db3ec0762de68e
Cornel Andreev <hugronaphor@gmail.com>
2015-01-11 22:00:05 +0000

    Kind of done.

--- themes/media-vine-video.tpl.php
+++ themes/media-vine-video.tpl.php
@@ -1,30 +1,28 @@
 <?php
 
 /**
- * @file media_vimeo/themes/media-vimeo-video.tpl.php
+ * @file 
+ * media_vimeo/themes/media-vimeo-video.tpl.php
  *
- * @todo Edit description when dev is done!!!
- * Template file for theme('media_vimeo_video').
+ * Template file for theme('media_vine_video').
  *
  * Variables available:
- *  $uri - The media uri for the Vimeo video (e.g., vimeo://v/xsy7x8c9).
- *  $video_id - The unique identifier of the Vimeo video (e.g., xsy7x8c9).
+ *  $uri - The media uri for the Vine video (e.g., vine://v/hrAOEOBw0F2 ).
+ *  $video_id - The unique identifier of the Vine video (e.g., hrAOEOBw0F2).
  *  $id - The file entity ID (fid).
- *  $url - The full url including query options for the Vimeo iframe.
- *  $options - An array containing the Media Vimeo formatter options.
- *  $api_id_attribute - An id attribute if the Javascript API is enabled;
- *  otherwise NULL.
+ *  $url - The full url including query options for the Vine iframe.
+ *  $options - An array containing the Media Vine formatter options.
  *  $width - The width value set in Media: Vimeo file display options.
  *  $height - The height value set in Media: Vimeo file display options.
- *  $title - The Media: YouTube file's title.
- *  $alternative_content - Text to display for browsers that don't support
- *  iframes.
- *
+ *  $title - The Media: YouTube file's title. * 
+ *  $embed_format - Vine embed Format.
+ *  $autoplay_audio - Vine autoplay audio.
+ * 
+ * 
+ *  $alternative_content - Text to display for browsers that don't support iframes. *
  */
-//dsm(get_defined_vars());
 ?>
 
-
-<div class="<?php print $classes; ?> oii-blea media-vine-<?php print $id; ?>">  
-  <iframe class="vine-embed" src="https://vine.co/v/<?php print $video_id; ?>/embed/simple" width="<?php print $width; ?>" height="<?php print $height; ?>" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
+<div class="<?php print $classes; ?> media-vine-<?php print $id; ?>">  
+  <iframe class="vine-embed" src="<?php print $url; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>" frameborder="0"><?php print $alternative_content; ?></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
 </div>
